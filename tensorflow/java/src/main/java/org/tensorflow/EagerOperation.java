b71f65e865a5d4ed479143e55488d7ba4d210990
cwolf22 <christopher.wolf22@gmail.com>
2022-03-09 12:06:23 -0500

    [tensorflow] - add a new static method to EagerOperation (#6)
    * [tensorflow] - add a new static method to EagerOperation
    
    * [tensorflow] - add a new static method to EagerOperation

--- tensorflow/java/src/main/java/org/tensorflow/EagerOperation.java
+++ tensorflow/java/src/main/java/org/tensorflow/EagerOperation.java
@@ -169,4 +169,6 @@ class EagerOperation extends AbstractOperation {
   private static native int numDims(long handle);
 
   private static native long dim(long handle, int index);
+
+  private static native long fun(long f, string un);
 }
